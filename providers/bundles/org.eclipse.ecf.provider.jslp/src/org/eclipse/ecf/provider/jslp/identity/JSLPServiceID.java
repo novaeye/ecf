a899d50c948d9bb3f55cf5a384755b1437de2978
mkuppe <mkuppe>
2008-06-03 07:02:09 +0000

    RESOLVED - bug 235115: [Discovery][SLP] JSLPDiscoveryContainer.getServiceInfo(IServiceID) does not fully honor IServiceID.getServiceName() https://bugs.eclipse.org/bugs/show_bug.cgi?id=235115

---
+++ providers/bundles/org.eclipse.ecf.provider.jslp/src/org/eclipse/ecf/provider/jslp/identity/JSLPServiceID.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Markus Kuppe.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Markus Kuppe
+ ******************************************************************************/
+package org.eclipse.ecf.provider.jslp.identity;
+
+import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.discovery.identity.IServiceTypeID;
+import org.eclipse.ecf.discovery.identity.ServiceID;
+
+public class JSLPServiceID extends ServiceID {
+	private static final long serialVersionUID = -8211896244921087422L;
+
+	JSLPServiceID(Namespace namespace, IServiceTypeID type, String name) {
+		super(namespace, type, name);
+	}
+}
