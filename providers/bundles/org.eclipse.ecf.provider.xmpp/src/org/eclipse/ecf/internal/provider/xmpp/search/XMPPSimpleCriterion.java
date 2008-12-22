b941683525b0074eb42a8c2f5c136cded11f8908
mmayworm <mmayworm>
2008-12-22 22:22:33 +0000

    [bug:256696] implementing user search on XMPP

---
+++ providers/bundles/org.eclipse.ecf.provider.xmpp/src/org/eclipse/ecf/internal/provider/xmpp/search/XMPPSimpleCriterion.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Marcelo Mayworm. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 	Marcelo Mayworm - initial API and implementation
+ *
+ ******************************************************************************/
+package org.eclipse.ecf.internal.provider.xmpp.search;
+
+import org.eclipse.ecf.presence.search.SimpleCriterion;
+
+public class XMPPSimpleCriterion extends SimpleCriterion {
+
+	public XMPPSimpleCriterion(String field, String value, String operator,
+			boolean ignoreCase) {
+		super(field, value, operator, ignoreCase);
+
+	}
+
+
+	public XMPPSimpleCriterion(String field, String value, String operator) {
+		super(field, value, operator);
+	}
+	
+
+	public String toExpression() {
+		return value;
+	}
+}
